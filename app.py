6f6545ca0f52f33695e89019a1c1acc9ca8af731
lauren <lauren444416@gmail.com>
2024-12-06 01:46:17 +0800

    v7

--- app.py
+++ app.py
@@ -1,69 +1,8 @@
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-#======python的函數庫==========
-import tempfile, os
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 import datetime
-import time
-import traceback
-import datetime
-#======python的函數庫==========
 
-app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    now = datetime.datetime.now()
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(f"現在時間{now}"))
-        
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+line_bot_api=LineBotApi('TT0Wqwwc4Sb///xBx6wagFJD3cozrSdpEXnaolKt2h2ks8UR5DZSnLSMYblKjkiH/80CtqEXNMcbceeXZq4K5eKaesR0KQoD0DiKYm6Cc6QDIDJcbk/uvv4/nbA/Jqlmj+ZBb4Z+RrbgxQl1UFFXfAdB04t89/1O/w1cDnyilFU=')
+UserID='U8a38390ef3f99dce49c43876e09f4912'
+text_message=TextSendMessage(text=str(datetime.datetime.now()))
+line_bot_api.push_message(UserID,text_message)
